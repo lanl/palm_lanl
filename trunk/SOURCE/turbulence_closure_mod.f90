2c7bca6fbf624c302da2e56e7b6c974e1bfc1e54
Joe <joe@fluidnumerics.com>
2019-03-15 17:33:18 +0000

    Parallelized tcm_prognostic in acc region

--- trunk/SOURCE/turbulence_closure_mod.f90
+++ trunk/SOURCE/turbulence_closure_mod.f90
@@ -2104,7 +2104,9 @@
        IF ( rans_tke_e )  produc = tend - advec
     
     !$acc data copy(drho_air(nzb+1:nzt),dd2zu(nzb+1:nzt),ddzu(nzb+1:nzt+1),ddzw(nzb+1:nzt),dissipation(nzb+1:nzt,nys:nyn),e(nzb:nzt+1,nys-1:nyn+1,nxl-1:nxr+1),e_p(nzb:nzt+1,nys-1:nyn+1,nxl-1:nxr+1),l_grid(nzb+1:nzt),l_wall(nzb+1:nzt,nys:nyn,nxl:nxr),g,kh(nzb+1:nzt,nysg:nyng,nxlg:nxrg),km(nzb:nzt+1,nys-1:nyn+1,nxl-1:nxr+1),prho(nzb:nzt+1,nys-1:nyn+1,nxl-1:nxr+1),prho_reference,rho_air_zw(nzb:nzt),tend(nzb+1:nzt,nys:nyn,nxl:nxr),tsc(3),te_m(nzb+1:nzt,nys:nyn,nxl:nxr),wall_flags_0(nzb+1:nzt,nys:nyn,nxl:nxr)) 
-    !$acc kernels default(present)
+
+    !$acc parallel default(present)
+    !$acc loop collapse(3)
 !
     !-- Calculate the tendency terms
        DO  i = nxl, nxr
@@ -2175,7 +2177,7 @@
    
        ENDDO
     
-    !$acc end kernels
+    !$acc end parallel
 
     !$acc end data
 

57922b478eb638e2d8121df5238cb0034a6291ad
Qing Li <lqingpku@gmail.com>
2019-05-15 11:34:32 -0600

    Working on exchange_horiz

--- trunk/SOURCE/exchange_horiz.f90
+++ trunk/SOURCE/exchange_horiz.f90
@@ -19,34 +19,34 @@
 !
 ! Current revisions:
 ! -----------------
-! 
-! 
+!
+!
 ! Former revisions:
 ! -----------------
 ! $Id: exchange_horiz.f90 2718 2018-01-02 08:49:38Z maronga $
 ! Corrected "Former revisions" section
-! 
+!
 ! 2696 2017-12-14 17:12:51Z kanani
 ! Change in file header (GPL part)
 ! 3D-Integer exchange on multigrid level (MS)
-! 
+!
 ! 2298 2017-06-29 09:28:18Z raasch
 ! sendrecv_in_background related parts removed
-! 
+!
 ! 2119 2017-01-17 16:51:50Z raasch
 !
 ! 2118 2017-01-17 16:38:49Z raasch
 ! OpenACC directives and related code removed
-! 
+!
 ! 2000 2016-08-20 18:09:15Z knoop
 ! Forced header and separation lines into 80 columns
-! 
+!
 ! 1804 2016-04-05 16:30:18Z maronga
 ! Removed code for parameter file check (__check)
-! 
+!
 ! 1682 2015-10-07 23:56:08Z knoop
-! Code annotations made doxygen readable 
-! 
+! Code annotations made doxygen readable
+!
 ! 1677 2015-10-02 13:25:23Z boeske
 ! Added new routine for exchange of three-dimensional integer arrays
 !
@@ -61,11 +61,11 @@
 !
 ! 1320 2014-03-20 08:40:49Z raasch
 ! ONLY-attribute added to USE-statements,
-! kind-parameters added to all INTEGER and REAL declaration statements, 
-! kinds are defined in new module kinds, 
+! kind-parameters added to all INTEGER and REAL declaration statements,
+! kinds are defined in new module kinds,
 ! revision history before 2012 removed,
 ! comment fields (!:) to be used for variable explanations added to
-! all variable declaration statements 
+! all variable declaration statements
 !
 ! 1257 2013-11-08 15:18:40Z raasch
 ! openacc loop and loop vector clauses removed
@@ -94,20 +94,20 @@
 !> lateral boundary conditions, respectively.
 !------------------------------------------------------------------------------!
  SUBROUTINE exchange_horiz( ar, nbgp_local)
- 
+
 
     USE control_parameters,                                                    &
         ONLY:  bc_lr, bc_lr_cyc, bc_ns, bc_ns_cyc, grid_level,                 &
                mg_switch_to_pe0, synchronous_exchange
-                
+
     USE cpulog,                                                                &
         ONLY:  cpu_log, log_point_s
-        
+
     USE indices,                                                               &
         ONLY:  nxl, nxr, nyn, nys, nzb, nzt
-        
+
     USE kinds
-    
+
     USE pegrid
 
     IMPLICIT NONE
@@ -117,10 +117,10 @@
     INTEGER(iwp) ::  j           !<
     INTEGER(iwp) ::  k           !<
     INTEGER(iwp) ::  nbgp_local  !<
-    
+
     REAL(wp), DIMENSION(nzb:nzt+1,nys-nbgp_local:nyn+nbgp_local,               &
                         nxl-nbgp_local:nxr+nbgp_local) ::  ar  !<
-                        
+
 
     CALL cpu_log( log_point_s(2), 'exchange_horiz', 'start' )
 
@@ -251,15 +251,41 @@
 !-- Case dependent, because in GPU mode still not all arrays are on device. This
 !-- workaround has to be removed later. Also, since PGI compiler 12.5 has problems
 !-- with array syntax, explicit loops are used.
-    IF ( bc_lr == 'cyclic' )  THEN
-       ar(:,:,nxl-nbgp_local:nxl-1) = ar(:,:,nxr-nbgp_local+1:nxr)
-       ar(:,:,nxr+1:nxr+nbgp_local) = ar(:,:,nxl:nxl+nbgp_local-1)
+    !$acc data copy( ar )
+    !$acc parallel present( ar )
+    IF  ( bc_lr == 'cyclic' )  THEN
+       ! ar(:,:,nxl-nbgp_local:nxl-1) = ar(:,:,nxr-nbgp_local+1:nxr)
+       ! ar(:,:,nxr+1:nxr+nbgp_local) = ar(:,:,nxl:nxl+nbgp_local-1)
+       !$acc loop collapse(2)
+       DO  j = nys-nbgp_local, nyn+nbgp_local
+          DO  k = nzb, nzt+1
+             !$acc loop seq
+             DO  i = 0, nbgp_local-1
+                ar(k,j,nxl-nbgp_local+i) = ar(k,j,nxr-nbgp_local+1+i)
+                ar(k,j,nxr+1+i)          = ar(k,j,nxl+i)
+             ENDDO
+          ENDDO
+       ENDDO
     ENDIF
+    !$acc end parallel
 
+    !$acc parallel present( ar )
     IF ( bc_ns == 'cyclic' )  THEN
-       ar(:,nys-nbgp_local:nys-1,:) = ar(:,nyn-nbgp_local+1:nyn,:)
-       ar(:,nyn+1:nyn+nbgp_local,:) = ar(:,nys:nys+nbgp_local-1,:)
+       ! ar(:,nys-nbgp_local:nys-1,:) = ar(:,nyn-nbgp_local+1:nyn,:)
+       ! ar(:,nyn+1:nyn+nbgp_local,:) = ar(:,nys:nys+nbgp_local-1,:)
+       !$acc loop collapse(2)
+       DO  i = nxl-nbgp_local, nxr+nbgp_local
+          DO  k = nzb, nzt+1
+             !$acc loop seq
+             DO  j = 0, nbgp_local-1
+                ar(k,nys-nbgp_local+j,i) = ar(k,nyn-nbgp_local+1+j,i)
+                ar(k,nyn+1+j,i)          = ar(k,nys+j,i)
+             ENDDO
+          ENDDO
+       ENDDO
     ENDIF
+    !$acc end parallel
+    !$acc end data
 
 #endif
     CALL cpu_log( log_point_s(2), 'exchange_horiz', 'stop' )
@@ -276,12 +302,12 @@
 
     USE control_parameters,                                                    &
         ONLY:  bc_lr, bc_lr_cyc, bc_ns, bc_ns_cyc, grid_level
-                       
+
     USE indices,                                                               &
         ONLY:  nxl, nxr, nyn, nys, nzb, nzt
-        
+
     USE kinds
-    
+
     USE pegrid
 
     IMPLICIT NONE
@@ -292,7 +318,7 @@
     INTEGER(iwp) ::  nys_l       !< local index bound at current grid level, south side
     INTEGER(iwp) ::  nzt_l       !< local index bound at current grid level, top
     INTEGER(iwp) ::  nbgp_local  !< number of ghost points
-    
+
     INTEGER(iwp), DIMENSION(nzb:nzt_l+1,nys_l-nbgp_local:nyn_l+nbgp_local,     &
                             nxl_l-nbgp_local:nxr_l+nbgp_local) ::  ar  !< treated array
 
